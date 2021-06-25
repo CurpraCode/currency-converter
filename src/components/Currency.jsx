72f68628df0321eccc59dc32d1852a57a3a37004
Oyakoya Curtis <40108842+CurpraCode@users.noreply.github.com>
2021-06-25 23:49:49 +0100

    Update Currency.jsx
    Working

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -24,8 +24,8 @@ function Currency() {
     fetch(BASE_URL)
       .then(res => res.json())
       .then(data => {
-        const firstCurrency = Object.keys(data.rates)[0]
-        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        const firstCurrency = Object?.keys(data.rates)[0]
+        setCurrencyOptions([data.base, ...Object?.keys(data.rates)])
         setFromCurrency(data.base)
         setToCurrency(firstCurrency)
         setExchangeRate(data.rates[firstCurrency])
