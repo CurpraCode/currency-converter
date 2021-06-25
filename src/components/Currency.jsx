55f31752d94b86cbe42d1ffa635b6de1f6fd40ed
Oyakoya Curtis <curtsoyaks@gmail.com>
2021-06-25 22:45:42 +0100

    first commit

---
+++ src/components/Currency.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import CurrencySet from './CurrencySet'
+
+const BASE_URL = 'https://api.exchangeratesapi.io/latest'
+
+function Currency() {
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
+  const [exchangeRate, setExchangeRate] = useState()
+  const [amount, setAmount] = useState(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+
+  let toAmount, fromAmount
+  if (amountInFromCurrency) {
+    fromAmount = amount
+    toAmount = amount * exchangeRate
+  } else {
+    toAmount = amount
+    fromAmount = amount / exchangeRate
+  }
+
+  useEffect(()=>{
+      axios.get(BASE_URL)
+      .then((res.data)=>{
+        const firstCurrency = Object.keys(data.rates)[0]
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        setFromCurrency(data.base)
+        setToCurrency(firstCurrency)
+        setExchangeRate(data.rates[firstCurrency])
+      })
+  }, [])
+
+  useEffect(()=>{
+      if(fromCurrency != null && toCurrency != null){
+          axios.get(`${base_url}?base=${fromCurrency}&symbols=${toCurrency}`)
+          .then((res.data)=>{
+              setExchangeRate(data.rates[toCurrency])
+          })
+      }
+  }, [fromCurrency, toCurrency])
+
+
+
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
+  }
+
+  function handleToAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
+  }
+
+  return (
+    <>
+      <h1>Convert</h1>
+      <CurrencySet
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={e => setFromCurrency(e.target.value)}
+        onChangeAmount={handleFromAmountChange}
+        amount={fromAmount}
+      />
+      <div className="equals">=</div>
+      <CurrencySet
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={e => setToCurrency(e.target.value)}
+        onChangeAmount={handleToAmountChange}
+        amount={toAmount}
+      />
+    </>
+  );
+}
+
+export default Currency;
